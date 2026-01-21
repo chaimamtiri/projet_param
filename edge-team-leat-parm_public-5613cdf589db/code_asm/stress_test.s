91155f0cc56d9f23cea29f92bf13877abd79a72f
EsserGhanmi <esser.ghanmi@etu.unice.fr>
2026-01-21 22:49:28 +0100

    Add files via upload

---
+++ edge-team-leat-parm_public-5613cdf589db/code_asm/stress_test.s
@@ -0,0 +1,19 @@
+
+.machin
+   B truc5   
+movs r0, #0
+     movs r1, #1
+     	@ bidule
+	bNE truc2
+		movs r7,  #5
+     TRUC1: TRUC2: TRUC3: movs r2, #2
+     
+   .TRUC4:
+  .chose
+TRUC5:
+
+	movs         r3,    #3
+	
+
+	BEQ .truc4
+	movs r6, #2
